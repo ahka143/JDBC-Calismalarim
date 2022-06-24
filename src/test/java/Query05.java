4878dd98faacb1197aab0aba88bfc121af046585
Ahmet Kaya <ahka1143@gmail.com>
2022-06-24 22:31:23 +0300

    24.06.2022

---
+++ src/test/java/Query05.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Query05 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/okul?serverTimezone=UTC", "root", "241143");
+
+
+        Statement st = con.createStatement();
+
+        // SORU: İşçiler adında bir tablo oluşturunuz. id int, birim VARCHAR(10), maas int
+        String sorgu = "create table isciler (id int , birim varchar (10), maas int)";
+        st.execute(sorgu);
+        System.out.println("isciler tablosu olusturuldu");
+    }
+}
--- src/test/java/Query4.java
+++ src/test/java/Query4.java
@@ -8,29 +8,31 @@ public class Query4 {
 
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/okul?serverTimezone=UTC", "root", "241143");
 
-        PreparedStatement pr=con.prepareStatement("select * from ogrenciler");
+        PreparedStatement pr = con.prepareStatement("select * from ogrenciler");
 
-        ResultSet rs=pr.executeQuery();
+        ResultSet rs = pr.executeQuery();
 
-        ResultSetMetaData rsmd=rs.getMetaData();
+        ResultSetMetaData rsmd = rs.getMetaData();
 
-        System.out.println("1. sutunun ismi: "+rsmd.getColumnName(1));
-        System.out.println("2. sutunun ismi: "+rsmd.getColumnName(2));
-        System.out.println("3. sutunun ismi: "+rsmd.getColumnName(3));
-        System.out.println("4. sutunun ismi: "+rsmd.getColumnName(4));
+        System.out.println("1. sutunun ismi: " + rsmd.getColumnName(1));
+        System.out.println("2. sutunun ismi: " + rsmd.getColumnName(2));
+        System.out.println("3. sutunun ismi: " + rsmd.getColumnName(3));
+        System.out.println("4. sutunun ismi: " + rsmd.getColumnName(4));
         System.out.println("======================================");
 
-        System.out.println("Toplam sutun sayisi: "+rsmd.getColumnCount());
+        System.out.println("Toplam sutun sayisi: " + rsmd.getColumnCount());
         System.out.println("======================================");
 
-        System.out.println("1.sutunun data tipi: "+rsmd.getColumnTypeName(1));
-        System.out.println("2.sutunun data tipi: "+rsmd.getColumnTypeName(2));
-        System.out.println("3.sutunun data tipi: "+rsmd.getColumnTypeName(3));
-        System.out.println("4.sutunun data tipi: "+rsmd.getColumnTypeName(4));
+        System.out.println("1.sutunun data tipi: " + rsmd.getColumnTypeName(1));
+        System.out.println("2.sutunun data tipi: " + rsmd.getColumnTypeName(2));
+        System.out.println("3.sutunun data tipi: " + rsmd.getColumnTypeName(3));
+        System.out.println("4.sutunun data tipi: " + rsmd.getColumnTypeName(4));
 
         System.out.println("======================================");
 
-        System.out.println("Tablonun ismi: "+rsmd.getTableName(1));
+        System.out.println("Tablonun ismi: " + rsmd.getTableName(1));
+
+        System.out.println("satir sayisi: "+ rsmd.getColumnTypeName(1).length());
 
     }
 }
