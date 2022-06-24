2577140be71b53d9c05d0be63596c9a185c67fe2
Ahmet Kaya <ahka1143@gmail.com>
2022-06-24 11:39:12 +0300

    24.06.2022

--- src/test/java/Query01.java
+++ src/test/java/Query01.java
@@ -16,5 +16,20 @@ public class Query01 {
 
         //4) ResultSet:
         ResultSet veri = st.executeQuery("select * from ogrenciler");
+        //5) Sonuclari al:
+        while (veri.next()){
+            System.out.println(veri.getInt(1)+" "+veri.getString(2)
+            +" "+veri.getString(3)+" "+veri.getString(4));
+            System.out.println("======================================================================");
+            System.out.println(veri.getInt("okul_no")+" "+veri.getString("ogrenci_ismi")
+                    +" "+veri.getString("sinif")+" "+veri.getString("cinsiyet"));
+            System.out.println("======================================================================");
+        }
+
+        //6) Kapatma:
+        con.close();
+        st.close();
+        veri.close();
+
     }
 }
