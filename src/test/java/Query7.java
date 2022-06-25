47370f3af268f010d6527c549cabfde21a5d72bc
Ahmet Kaya <ahka1143@gmail.com>
2022-06-25 21:42:30 +0300

    25.06.2022

---
+++ src/test/java/Query7.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Query7 {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, SQLException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection con =  DriverManager.getConnection("jdbc:mysql://localhost:3306/okul?serverTimezone=UTC", "root", "Murat34+");
+
+
+        // STATEMENT, PREPAREDSTATEMENT FARKI
+
+        // STATEMENT:
+        //Statement st = con.createStatement();
+        //st.executeUpdate("insert into ogrenciler values (200, 'Ali Can', 10, 'E')");
+
+        // PREPAREDSTATEMENT: ifadeler parametrre kullanilarak yollanabilir.
+        PreparedStatement ps = con.prepareStatement("insert into ogrenciler values (?, ?, ? ,?)");
+        ps.setInt(1,201);
+        ps.setString(2,"Veli Can");
+        ps.setString(3,"12");
+        ps.setString(4,"E");
+
+        ps.executeUpdate();
+        System.out.println("Veri girisi yapildi.");
+    }
+}
