0ef0bb23c085fa6af30b9876a19d5c33031307a2
Ahmet Kaya <ahka1143@gmail.com>
2022-07-08 22:42:52 +0300

    8.7.22

---
+++ src/test/java/QueryCalisma04.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class QueryCalisma04 {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/batch59?serverTimezone=UTC", "root", "241143");
+        Statement st = con.createStatement();
+    }
+}
