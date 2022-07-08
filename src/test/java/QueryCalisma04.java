242ee9d9eb5af3111f95577b8fafa3f7ab25d104
Ahmet Kaya <ahka1143@gmail.com>
2022-07-08 23:17:15 +0300

    8.7.22

--- src/test/java/QueryCalisma04.java
+++ src/test/java/QueryCalisma04.java
@@ -1,13 +1,18 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class QueryCalisma04 {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/batch59?serverTimezone=UTC", "root", "241143");
-        Statement st = con.createStatement();
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/okul?serverTimezone=UTC", "root", "241143");
+
+        PreparedStatement ps=con.prepareStatement("insert into ogrenciler values(?,?,?,?)");
+
+
+        ps.setString(1,"333");
+        ps.setString(2,"ahmet");
+        ps.setString(3,"Veli");
+        ps.setInt(4,98);
+        ps.execute();
     }
 }
