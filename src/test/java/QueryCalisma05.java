374fcdff4e6f519cc9e2cde232d19562fa41f216
Ahmet Kaya <ahka1143@gmail.com>
2022-07-08 23:37:46 +0300

    8.7.22

--- src/test/java/QueryCalisma05.java
+++ src/test/java/QueryCalisma05.java
@@ -8,5 +8,20 @@ public class QueryCalisma05 {
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/batch59?serverTimezone=UTC", "root", "241143");
         Statement st = con.createStatement();
+
+        // SORU1: Urunler adında bir tablo olusturalım (id int, isim varchar(10), fiyat int)
+
+       // st.execute("create table urunler(id int, isim varchar(10), fiyat int);");
+
+        // SORU2: urunler tablosuna aşağıdaki verileri toplu bir şekilde etkileyin
+        // Çok miktarda kayıt eklemek için PreparedStatement metodu kullanılır.
+        // 1) Veri girişine uygun bir POJO (Plain Old Java Object) class oluşturulur
+        // 2) POJO class ta nesneleri saklayacak bir collection oluşturulur
+        // 3) Bir döngü ile kayıtlar eklenir.
+
+
+
+
+
     }
 }
